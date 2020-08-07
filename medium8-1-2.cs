61fce3462576070481c5abf6145e21612183e0be
adavletyarov <adavletyarov@fors.ru>
2020-08-07 21:47:56 +0300

    Решение

--- medium8-1-2.cs
+++ medium8-1-2.cs
@@ -3,85 +3,127 @@ using System.Collections.Generic;
 
 namespace Task
 {
-    class Program
+    static class Program
     {
-        public static void Main(string[] args)
+        private static void Main()
         {
-            int obj1x = 5;
-            int obj1y = 5;
-            bool isalive1 = true;
-            int obj2x = 10;
-            int obj2y = 10;
-            bool isalive2 = true;
-            int obj3x = 15;
-            int obj3y = 15;
-            bool isalive3 = true;
-
-            Random random = new Random();
+            GameObjectList gameObjectList = new GameObjectList();
+            gameObjectList.Add(new GameObject(5, 5, "1", true));
+            gameObjectList.Add(new GameObject(10, 10, "2", true));
+            gameObjectList.Add(new GameObject(15, 15, "3", true));
 
             while (true)
             {
-                if (obj1x == obj2x && obj1y == obj2y)
+                gameObjectList.KillAllWithCollision();
+                gameObjectList.MoveAll();
+                gameObjectList.PrintAllAlive();
+            }
+        }
+    }
+
+    public class Coordinate
+    {
+        public int Value { private set; get; }
+
+        public Coordinate(int coordinate)
+        {
+            Value = coordinate;
+        }
+
+        public void RandomStep(Random random)
+        {
+            Value += random.Next(-1, 1);
+            if (Value < 0)
+            {
+                Value = 0;
+            }
+        }
+
+        public bool Equals(Coordinate coordinate)
+        {
+            return Value == coordinate.Value;
+        }
+    }
+
+    public class GameObject
+    {
+        private readonly Coordinate _x;
+        private readonly Coordinate _y;
+        private readonly string _name;
+
+        public bool IsAlive { private set; get; }
+
+        public GameObject(int x, int y, string name, bool isAlive)
+        {
+            _x = new Coordinate(x);
+            _y = new Coordinate(y);
+            _name = name;
+            IsAlive = isAlive;
+        }
+
+        public void Kill()
+        {
+            IsAlive = false;
+        }
+
+        public void RandomStep(Random random)
+        {
+            _x.RandomStep(random);
+            _y.RandomStep(random);
+        }
+
+        public bool IsCollisionWith(GameObject gameObject)
+        {
+            return _x.Equals(gameObject._x) && _y.Equals(gameObject._y);
+        }
+
+        public void Print()
+        {
+            Console.SetCursorPosition(_x.Value, _y.Value);
+            Console.Write(_name);
+        }
+    }
+
+    public class GameObjectList
+    {
+        private readonly List<GameObject> _items = new List<GameObject>();
+        private readonly Random _random = new Random();
+
+        public void Add(GameObject gameObject)
+        {
+            _items.Add(gameObject);
+        }
+
+        public void MoveAll()
+        {
+            foreach (var gameObject in _items)
+            {
+                gameObject.RandomStep(_random);
+            }
+        }
+
+        public void KillAllWithCollision()
+        {
+            for (int i = 0; i < _items.Count; i++)
+            {
+                for (int j = i + 1; j < _items.Count; j++)
                 {
-                    isalive1 = false;
-                    isalive2 = false;
+                    if (_items[i].IsCollisionWith(_items[j]))
+                    {
+                        _items[i].Kill();
+                        _items[j].Kill();
+                    }
                 }
+            }
+        }
 
-                if (obj1x == obj3x && obj1y == obj3y)
+        public void PrintAllAlive()
+        {
+            foreach (var gameObject in _items)
+            {
+                if (gameObject.IsAlive)
                 {
-                    isalive1 = false;
-                    isalive3 = false;
-                }
-
-                if (obj2x == obj3x && obj2y == obj3y)
-                {
-                    isalive2 = false;
-                    isalive3 = false;
-                }
-
-                obj1x += random.Next(-1, 1);
-                obj1y += random.Next(-1, 1);
-
-                obj2x += random.Next(-1, 1);
-                obj2y += random.Next(-1, 1);
-
-                obj3x += random.Next(-1, 1);
-                obj3y += random.Next(-1, 1);
-
-                if (obj1x < 0)
-                    obj1x = 0;
-
-                if (obj1y < 0)
-                    obj1y = 0;
-
-                if (obj2x < 0)
-                    obj2x = 0;
-
-                if (obj2y < 0)
-                    obj2y = 0;
-
-                if (obj3x < 0)
-                    obj3x = 0;
-
-                if (obj3y < 0)
-                    obj3y = 0;
-
-                if (isalive1)
-                {
-                    Console.SetCursorPosition(obj1x, obj1y);
-                    Console.Write("1");
-                }
-
-                if (isalive2)
-                {
-                    Console.SetCursorPosition(obj2x, obj2y);
-                    Console.Write("2");
-                }
-
-                if (isalive3)
-                {
-                    Console.SetCursorPosition(obj3x, obj3y);
-                    Console.Write("3");
+                    gameObject.Print();
                 }
             }
         }
